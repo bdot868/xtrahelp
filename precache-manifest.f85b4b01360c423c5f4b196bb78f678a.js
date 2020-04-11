a84a6edd04f20131beb5bbdd503b8167fb8498c0
bdot868 <brandoncarter100@gmail.com>
2020-04-11 11:10:27 -0700

    Updates

---
+++ precache-manifest.f85b4b01360c423c5f4b196bb78f678a.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "98f8631c391b3014fc4b0647d0347d63",
+    "url": "/xtrahelp/index.html"
+  },
+  {
+    "revision": "40ab502bb29c031dadc2",
+    "url": "/xtrahelp/static/js/2.dc818d9e.chunk.js"
+  },
+  {
+    "revision": "4d47ffde3dc69784e8e23fd910817ef8",
+    "url": "/xtrahelp/static/js/2.dc818d9e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "19a1be3e991eebcd1938",
+    "url": "/xtrahelp/static/js/main.c1ec835c.chunk.js"
+  },
+  {
+    "revision": "502b8edccab30382c013",
+    "url": "/xtrahelp/static/js/runtime-main.93f89410.js"
+  }
+]);
